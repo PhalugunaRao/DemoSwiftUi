21abc733d9c4607eea757dc87e792be4ec3d84e0
PhalgunaRao <phalu900@gmail.com>
2022-12-20 14:13:26 +0530

    Initial Commit

---
+++ DemoSwiftUi/DemoSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUiApp.swift
+//  DemoSwiftUi
+//
+//  Created by ekincare on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
